c8ac80d3d43a3abe28c802ba3bacf331e426d4ab
ITWILL <ITWILL@DESKTOP-PF5SGM6>
2022-08-22 11:52:22 +0900

    답안1

--- lab03_control/src/edu/java/loop05/LoopMain05.java
+++ lab03_control/src/edu/java/loop05/LoopMain05.java
@@ -5,18 +5,21 @@ public class LoopMain05 {
     public static void main(String[] args) {
         // 369 게임 출력
        
-       for (int n = 1; n <= 100 ; n++) {
-        	if (n % 10 == 1) {
+       for (int i = 1; i <= 100 ; i++) {
+           // 1의 자릿수 - 10으로 나눈 나머지
+           int n1 = i % 10;
+           // 10의 자릿수 - 10으로 나눈 몫
+           int n10 = i / 10;
+           
+           if (n1 == 3 || n1 == 6 || n1 == 9 || n10 == 3 || n10 == 6 || n10 == 9) {
+               System.out.print("*\t");  
+           } else {
+               System.out.print(i + "\t");
+           }
+        	
+           if (i % 10 == 0) {
                 System.out.println();
         	}
-        	if (n % 10 == 3 || n % 10 == 6 || n % 10 == 9) {
-                System.out.print("*" + "\t");
-            } else if (n >= 30 && n <= 39 || n >= 60 && n <= 69 || n >= 90 && n <= 99) {
-            	System.out.print("*" + "\t");
-            } else {
-            	System.out.print(n + "\t"); 
-            }
-        	
         	
         }   	
                 
